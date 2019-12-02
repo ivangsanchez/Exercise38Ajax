e9f776a988c731166436662f3ab3c4b2309ccfba
Mabestron <ivangamaliel555@hotmail.com>
2019-12-02 16:58:26 -0600

    Ajax v2

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,6 +1,8 @@
 /**
  *  Código para hacer peticiones
  */
+var tablaPerro =document.getElementById("tablaPerros");
+console.log(tablaPerro);
 
 function traerdatosperro(){
 	//crear un objeto para realizar peticion
@@ -27,5 +29,48 @@ function traerdatosperro(){
 
 function visualizarDatos(data)
 {
-	console.log(data);
+	var table =document.getElementById("tablaPerros");
+	var listaPerros =JSON.parse(data);
+	for(i=0;i<listaPerros.length;i++){
+		var perro = listaPerros[i];
+		crearFila(perro);
+	}
 }
+
+function crearFila(perro){
+	//CREACION DE ELEMENTO DE LA VISTA//
+	
+	//recuperando el tbody de la tabla
+	var tbody=tablaPerros.tBodies[0];
+	
+	//crear fila
+	var row = document.createElement("tr");
+	
+	// crear celdas para los datos de perro
+	var tdNombre= document.createElement("td");
+	var tdEdad= document.createElement("td");
+	var tdPeso= document.createElement("td");
+	var tdVivo= document.createElement("td");
+	var tdPropietario= document.createElement("td");
+	var tdCuidado= document.createElement("td");
+	
+	//Configuración de los datos
+	tdNombre.innerText=perro.perro;
+	tdEdad.innerText=perro.edad;
+	tdPeso.innerText=perro.peso;
+	tdVivo.innerText=perro.estaVivo;
+	tdPropietario.innerText=perro.propietario.nombre;
+	tdCuidado.innerText=perro.cuidados[0];
+	
+	//Agregar las celdas con datos a la fila
+	row.appendChild(tdNombre);
+	row.appendChild(tdEdad);
+	row.appendChild(tdPeso);
+	row.appendChild(tdVivo);
+	row.appendChild(tdPropietario);
+	row.appendChild(tdCuidado);
+	
+	//agregar la fila a la tabla
+	tbody.appendChild(row);
+}
+
